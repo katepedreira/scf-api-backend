63296c43a2d9671d53e188aa9f1136fe7651f136
Katherine.Pedreira <kakamarpe@gmail.com>
2023-06-17 00:09:33 -0300

    corrigindo erro

--- src/main/java/com/example/scfapi/resource/AuthResource.java
+++ src/main/java/com/example/scfapi/resource/AuthResource.java
@@ -1,4 +1,4 @@
-package com.academify.resource;
+package com.example.scfapi.resource;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
